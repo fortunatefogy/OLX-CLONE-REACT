8254059eb3cc50bde4330dac18aca37c73186614
subinraj <subinrajsofficial@gmail.com>
2024-12-28 11:43:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router,Route } from 'react-router-dom'
 /**
  * ?  =====Import Components=====
  */
@@ -9,7 +9,12 @@ import Home from './Pages/Home';
 function App() {
   return (
     <div>
+      <Router>
+        <Route path="/" component={Home} />
+
       <Home />
+
+      </Router>
     </div>
   );
 }
